a2d576ce92baebaae395d697607ce6d1c7eed23a
your_username <2429016980@qq.com>
2023-12-26 01:03:57 +0800

    176-Vue3大事件项目-添加分类-显示弹层封装弹层组件

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -2,11 +2,13 @@
 import { ref } from 'vue'
 import { artGetChannelsService } from '@/api/article/article.js'
 import { Edit, Delete } from '@element-plus/icons-vue'
+import ChannelEdit from './components/ChannelEdit.vue'
 
 // 表单数组
 const channelList = ref([])
 
 const loading = ref(false) //定义加载状态
+const dialog = ref() //定义弹窗
 
 const getChannelList = async () => {
   loading.value = true // 开始加载动画
@@ -16,26 +18,25 @@ const getChannelList = async () => {
 }
 getChannelList()
 
-const onEditchannel = (row, $index) => {
+const onDeletechannel = (row, $index) => {
   console.log(row, $index)
 }
 
-const onDeletechannel = (row, $index) => {
-  console.log(row, $index)
+const onEditchannel = (row) => {
+  dialog.value.open({ row })
+}
+
+const onaddChannel = () => {
+  dialog.value.open({})
 }
 </script>
 
 <template>
   <PageContainer title="文章分类">
     <template #extra>
-      <el-button tpye="primary"> 添加分类 </el-button>
+      <el-button @click="onaddChannel" tpye="primary"> 添加分类 </el-button>
     </template>
-    <el-table
-      v-if="channelList.length > 0"
-      v-loading="loading"
-      :data="channelList"
-      style="width: 100%"
-    >
+    <el-table v-loading="loading" :data="channelList" style="width: 100%">
       <el-table-column type="index" label="序号" width="100"></el-table-column>
       <el-table-column prop="cate_name" label="分类名称"></el-table-column>
       <el-table-column prop="cate_alias" label="分类别名"></el-table-column>
@@ -62,6 +63,9 @@ const onDeletechannel = (row, $index) => {
         <el-empty description="暂无数据" />
       </template>
     </el-table>
+
+    <!-- Dialog 对话框弹层组件 -->
+    <ChannelEdit ref="dialog"></ChannelEdit>
   </PageContainer>
 </template>
 
---
+++ src/views/article/components/ChannelEdit.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+const dialogVisible = ref(false) // 定义对话框显示状态
+
+const open = (row) => {
+  dialogVisible.value = true
+  console.log(row)
+}
+
+// 向外暴露方法
+defineExpose({
+  open
+})
+</script>
+
+<template>
+  <!-- Dialog 对话框弹层 -->
+  <el-dialog v-model="dialogVisible" title="Tips" width="30%">
+    <span>This is a message</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          确认
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<style lang="scss" scoped></style>
