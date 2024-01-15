308c74c82737c5cd43721c53c76a4241513b033f
your_username <2429016980@qq.com>
2024-01-15 14:03:57 +0800

    修改密码和重置

--- src/api/user.js
+++ src/api/user.js
@@ -18,5 +18,11 @@ export const userUpdateInfoService = ({ id, nickname, email }) =>
   request.put('/my/userinfo', { id, nickname, email })
 
 // 上传头像接口
+// 接口地址：https://apifox.com/apidoc/shared-26c67aee-0233-4d23-aab7-08448fdf95ff/api-93850054
 export const userUploadAvatarService = (avatar) =>
   request.patch('/my/update/avatar', { avatar })
+
+// 修改密码接口
+// 接口地址：https://apifox.com/apidoc/shared-26c67aee-0233-4d23-aab7-08448fdf95ff/api-93850055
+export const userUpdatePassService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
--- src/views/user/UserPassword.vue
+++ src/views/user/UserPassword.vue
@@ -1,26 +1,69 @@
 <script setup>
 import { ref } from 'vue'
+import { userUpdatePassService } from '@/api/user.js'
+// 引入 Vue Router
+import { useRouter } from 'vue-router'
+
+import { useUserStore } from '@/stores' //导入stores下面的用户仓库
+
+const formRef = ref()
+const router = useRouter()
+const userStore = useUserStore()
+
+const onSubmit = async () => {
+  // 验证表单
+  const valid = await formRef.value.validate()
+  // 如果表单验证通过
+  if (valid) {
+    // 调用用户更新密码服务
+    await userUpdatePassService(pwdForm.value)
+    // 弹出提示框
+    ElMessage({ type: 'success', message: '更换密码成功' })
+    // 更新用户token和用户信息
+    userStore.setToken('')
+    userStore.setUser({})
+    // 跳转到登录页
+    router.push('/login')
+  }
+}
+
+const onReset = () => {
+  // 重置表单
+  formRef.value.resetFields()
+}
+
+// 声明一个ref对象，用于重置密码表单数据
 const pwdForm = ref({
+  // 旧密码
   old_pwd: '',
+  // 新密码
   new_pwd: '',
+  // 确认新密码
   re_pwd: ''
 })
 
 const checkOldSame = (rule, value, cb) => {
+  // 检查新密码是否和原密码相同
   if (value === pwdForm.value.old_pwd) {
+    // 如果相同，则报错
     cb(new Error('原密码和新密码不能一样!'))
   } else {
+    // 如果不同，则通过
     cb()
   }
 }
 
 const checkNewSame = (rule, value, cb) => {
+  // 检查新密码和确认新密码是否一致
   if (value !== pwdForm.value.new_pwd) {
+    // 不一致则报错
     cb(new Error('新密码和确认再次输入的新密码不一样!'))
   } else {
+    // 一致则通过
     cb()
   }
 }
+
 const rules = {
   // 原密码
   old_pwd: [
@@ -51,12 +94,13 @@ const rules = {
     },
     { validator: checkNewSame, trigger: 'blur' }
   ]
-}
+} // 表单校验规则
 </script>
+
 <template>
   <page-container title="重置密码">
     <el-row>
-      <el-col :span="12">
+      <el-col :span="6">
         <el-form
           :model="pwdForm"
           :rules="rules"
@@ -64,13 +108,13 @@ const rules = {
           label-width="100px"
           size="large"
         >
-          <el-form-item label="原密码" prop="old_pwd">
+          <el-form-item label="原始密码：" prop="old_pwd">
             <el-input v-model="pwdForm.old_pwd" type="password"></el-input>
           </el-form-item>
-          <el-form-item label="新密码" prop="new_pwd">
+          <el-form-item label="新的密码：" prop="new_pwd">
             <el-input v-model="pwdForm.new_pwd" type="password"></el-input>
           </el-form-item>
-          <el-form-item label="确认新密码" prop="re_pwd">
+          <el-form-item label="确认密码：" prop="re_pwd">
             <el-input v-model="pwdForm.re_pwd" type="password"></el-input>
           </el-form-item>
           <el-form-item>
